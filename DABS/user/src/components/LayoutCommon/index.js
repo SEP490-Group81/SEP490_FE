c0dbe539986638804d8224afa941821fa388ba23
NguyenLap <lapnq>
2025-05-25 23:28:08 +0700

    Add menu item/ subitem for header user

--- DABS/user/src/components/LayoutCommon/index.js
+++ DABS/user/src/components/LayoutCommon/index.js
@@ -2,7 +2,9 @@ import { Outlet } from "react-router-dom";
 import { Button, Layout } from 'antd';
 import "./style.scss";
 import { UserOutlined, CaretDownOutlined, TikTokOutlined, FacebookOutlined, YoutubeOutlined } from '@ant-design/icons';
+import { Menu } from "antd";
 
+const { SubMenu } = Menu;
 const { Header, Footer } = Layout;
 function LayoutCommon() {
     return <>
@@ -28,60 +30,82 @@ function LayoutCommon() {
 
                         </div>
                         <div className="header__content__top__wrapper">
-                              <div className="header__content__top__wrapper__account">
-                            <Button type="primary"><UserOutlined />Tài khoản</Button>
+                            <div className="header__content__top__wrapper__account">
+                                <Button type="primary"><UserOutlined />Tài khoản</Button>
+                            </div>
                         </div>
-                        </div>
-                      
+
                     </div>
                     <div className="header__content__bottom">
-                        <ul>
+                        <Menu mode="horizontal">
+                            <Menu.Item key="support">
+                                <a href="/">Hỗ trợ đặt khám</a>
+                            </Menu.Item>
 
-                            <li>
-                                <div className="header__content__bottom__support">
-                                    <a href="/">Hỗ trợ đặt khám</a>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="header__content__bottom__content-component-1">
-                                    <a href="/">Cơ sở y tế</a>
-                                    <div><CaretDownOutlined /></div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="header__content__bottom__content-component-2">
-                                    <a href="/">Dịch vụ y tế</a>
-                                    <div><CaretDownOutlined /></div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="header__content__bottom__content-component-3">
-                                    <a href="/">Khám sức khoẻ doanh nghiệp</a>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="header__content__bottom__content-component-3">
-                                    <a href="/">Tin tức</a>
-                                    <div><CaretDownOutlined /></div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="header__content__bottom__content-component-3">
-                                    <a href="/">Hướng dẫn</a>
-                                    <div><CaretDownOutlined /></div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="header__content__bottom__content-component-3">
-                                    <a href="/">Liên hệ hợp tác</a>
-                                    <div><CaretDownOutlined /></div>
-                                </div>
-                            </li>
+                            <SubMenu
+                                key="medical-facilities"
+                                title={
+                                    <span>
+                                        Cơ sở y tế <CaretDownOutlined />
+                                    </span>
+                                }
+                            >
+                                <Menu.Item key="facility-1">Cơ sở 1</Menu.Item>
+                                <Menu.Item key="facility-2">Cơ sở 2</Menu.Item>
+                            </SubMenu>
 
-                        </ul>
+                            <SubMenu
+                                key="medical-services"
+                                title={
+                                    <span>
+                                        Dịch vụ y tế <CaretDownOutlined />
+                                    </span>
+                                }
+                            >
+                                <Menu.Item key="service-1">Dịch vụ 1</Menu.Item>
+                                <Menu.Item key="service-2">Dịch vụ 2</Menu.Item>
+                            </SubMenu>
 
+                            <Menu.Item key="enterprise-health">
+                                <a href="/">Khám sức khoẻ doanh nghiệp</a>
+                            </Menu.Item>
 
+                            <SubMenu
+                                key="news"
+                                title={
+                                    <span>
+                                        Tin tức <CaretDownOutlined />
+                                    </span>
+                                }
+                            >
+                                <Menu.Item key="news-1">Tin 1</Menu.Item>
+                                <Menu.Item key="news-2">Tin 2</Menu.Item>
+                            </SubMenu>
 
+                            <SubMenu
+                                key="guide"
+                                title={
+                                    <span>
+                                        Hướng dẫn <CaretDownOutlined />
+                                    </span>
+                                }
+                            >
+                                <Menu.Item key="guide-1">Hướng dẫn 1</Menu.Item>
+                                <Menu.Item key="guide-2">Hướng dẫn 2</Menu.Item>
+                            </SubMenu>
+
+                            <SubMenu
+                                key="contact"
+                                title={
+                                    <span>
+                                        Liên hệ hợp tác <CaretDownOutlined />
+                                    </span>
+                                }
+                            >
+                                <Menu.Item key="contact-1">Liên hệ 1</Menu.Item>
+                                <Menu.Item key="contact-2">Liên hệ 2</Menu.Item>
+                            </SubMenu>
+                        </Menu>
                     </div>
                 </div>
 
