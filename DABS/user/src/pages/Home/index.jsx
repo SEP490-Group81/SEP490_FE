0f6de2ada03a5c37cdbb8e84fdb5144ffdebb35f
NguyenLap <lapnq>
2025-06-04 14:59:33 +0700

    Create home page

--- DABS/user/src/pages/Home/index.jsx
+++ DABS/user/src/pages/Home/index.jsx
@@ -1,6 +1,33 @@
+
+import { Button, Col, Flex, Row, Tooltip } from "antd";
+import { Input, Space } from 'antd';
+import "./style.scss";
+
+const { Search } = Input;
+
 function Home() {
+    const onSearch = (value, _e, info) =>
+        console.log(info === null || info === void 0 ? void 0 : info.source, value);
     return <>
-    
+        <Row justify="center" style={{ marginBottom: 20 }}>
+            <Col className="gutter-row" span={12} style={{ textAlign: 'center' }}>
+                <h1>Kết nối Người Dân với Cơ sở & Dịch vụ Y tế hàng đầu</h1>
+            </Col>
+        </Row>
+        <Row justify="center">
+            <Col className="gutter-row" span={12} style={{ textAlign: 'center' }}>
+
+                <Search
+                    placeholder="Tìm kiếm bác sĩ, gói khám, cơ sở khám, ..."
+                    allowClear
+                    enterButton="Search"
+                    size="large"
+                    onSearch={onSearch}
+
+                />
+
+            </Col>
+        </Row>
     </>
 }
 export default Home;
